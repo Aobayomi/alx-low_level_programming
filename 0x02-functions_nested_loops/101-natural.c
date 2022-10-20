887ba2eaad5ad0dcf77ae86c70016882ad0c8c84
Aobayomi <abayomiakanbi21@gmail.com>
2022-10-19 21:47:39 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - list all the natural num < 1024 mult of 3 or 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int 1, sum = 0;
+
+	for (1 = 0; 1 < 1024; 1++)
+	{
+		if ((1 % 3) == 0 || (1 % 5) == 0)
+			sum += 1;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
